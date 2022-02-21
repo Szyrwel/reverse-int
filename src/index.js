041377356974d2d67de791eac64c07c6351b28ab
Szyrwel <potato_father@list.ru>
2022-02-21 11:38:10 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n)
+  let newN = n.toString();
+  let result = '';
+  for (let i = 0; i < newN.length; i++) {
+      result = newN[i] + result
+  }
+  n = result
+  return n
 }
